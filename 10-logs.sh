24f5e6cc69f27f11ba780d592aa4b9875b28cbb9
Dinakar <dinakargaddam23@gmail.com>
2026-05-25 23:05:02 +0530

    special variables

---
+++ 10-logs.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+USERID=$(id -u)
+LOGFS_DIR=/home/ec2-user/shell-logs
+LOGS_FILE="$LOGS_DIR/$0.log" # /home/ec2-user/shell-logs/10-shell-logs
+
+
+#checking the root access or not
+if [ "$USERID" -ne 0 ]; then
+    echo "Please run this script with root access !!!"
+    exit 1
+fi
+
+# First arg -> What are you trying to install
+# second arg -> exit code
+
+VALIDATE(){
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is FAILED ..."
+    else
+        echo "Installing $1 is SUCCESS ..."
+    fi
+
+}
+
+# echo "I'm continuing ..." 
+dnf list installed mysql &>> $LOGS_FILE  # here we are cheking the Mysql is installed or not
+if [ $? -eq 0 ] ; then # if the process is not equal to 0 , It means already installed
+    echo "Mysql is already installed ... SKIPPING"
+else # If the process is 0 , Now it will Install the Mysql
+    echo "Installing MySql"
+    dnf install mysql -y &>> $LOGS_FILE
+    VALIDATE MySql $!
+    
+fi
+
+dnf list installed nginx &>> $LOGS_FILE # here we are cheking the Mysql is installed or not
+if [ $? -eq 0 ] ; then # if the process is not equal to 0 , It means already installed
+    echo "Mysql is already installed ... SKIPPING"
+else # If the process is 0 , Now it will Install the Mysql
+    echo "Installing nginx"
+    dnf install nginx -y &>> $LOGS_FILE
+    VALIDATE nginx $!
+    
+fi
