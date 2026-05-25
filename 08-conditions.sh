5fd0ce39a4858a3965d167882d13109cacd59cd4
Dinakar <dinakargaddam23@gmail.com>
2026-05-25 21:17:19 +0530

    special variables

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -3,7 +3,7 @@ NUMBER=$1
 if [ "$NUMBER" -gt 20 ]; then
     echo "The given $NUMBER is greater than  20"
 
-elif ["$NUMBER" -eq 20]; then
+elif [ "$NUMBER" -eq 20 ]; then
     echo "The number $NUMBER is equals to 20 "
 else 
     echo "The given number $NUMBER is less than 20"
