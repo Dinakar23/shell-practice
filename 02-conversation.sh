e4b7a7ecbf3d1e1c2c6d30dab8cb25f27b8cafe8
Dinakar <dinakargaddam23@gmail.com>
2026-05-25 10:20:13 +0530

    changes with variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,8 +1,8 @@
 #!bin/bash
-person1= Trumph
-person2= Iran
+PERSON1= Trumph
+PERSON2= Iran
 
-echo "$person1   : Hey, $person2 we are hoing to blast you"
-echo "$person2   : You're Welcom Trumph "
-echo "$person1   : Hey $person2, I'm very serious"
-echo "$person2   : $person1 we are also very serious" 
+echo "$PERSON1   : Hey, $PERSON2 we are hoing to blast you"
+echo "$PERSON2   : You're Welcom Trumph "
+echo "$PERSON1   : Hey $PERSON2, I'm very serious"
+echo "$PERSON2   : $PERSON1 we are also very serious" 
