46a470af90ba0f76f45954addb7f64727d492623
Dinakar <dinakargaddam23@gmail.com>
2026-05-25 10:48:59 +0530

    changes with variables

--- 04-secure-enter.sh
+++ 04-secure-enter.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "enter your username ::"
+echo "enter your username :"
 read USER_NAME #here USER_NAME is variable , whatever you enter in the terminal it will store
-
 echo "username is $USER_NAME"
+
+echo "enter your password :"
+read -s PASSWD 
+echo $PASSWD
