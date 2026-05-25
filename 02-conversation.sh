f9a2d9c1765f611f70ccdb4bdd08c1065c6daf9a
Dinakar <dinakargaddam23@gmail.com>
2026-05-25 10:03:50 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+echo "Trumph : Hey, Iran we are hoing to blast you"
+echo "Iran   : You're Welcom Trumph "
+echo "Trumph : Hey, I'm very serious"
+echo "Iran   : Trumph we are also very serious" 
