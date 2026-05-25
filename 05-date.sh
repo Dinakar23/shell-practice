5786d22c5934adc94916da4f5c80d9634b119f9f
Dinakar <dinakargaddam23@gmail.com>
2026-05-25 11:14:49 +0530

    changes with variables

--- 05-date.sh
+++ 05-date.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-timestamp=$(date)
-echo "today time is :$timestamp"
+#timestamp=$(date)
+#echo "today time is :$timestamp"
+
+START_TIME=$(date +%s)
+sleep 5 
+END_TIME=$=(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo "Total time in seconds : $TOTAL_TIME"
