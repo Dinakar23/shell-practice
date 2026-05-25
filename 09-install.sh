a33bbed52a7cd64c091459b3c04204e65c2635d4
Dinakar <dinakargaddam23@gmail.com>
2026-05-25 22:45:21 +0530

    special variables

--- 09-install.sh
+++ 09-install.sh
@@ -7,18 +7,35 @@ if [ "$USERID" -ne 0 ]; then
     exit 1
 fi
 
+# First arg -> What are you trying to install
+# second arg -> exit code
+
+VALIDATE()={
+    if [ $2 -ne 0 ]; then
+        echo "Installing $1 is FAILED ..."
+    else
+        echo "Installing $1 is SUCCESS ..."
+    fi
+
+}
+
 # echo "I'm continuing ..." 
 dnf list installed mysql # here we are cheking the Mysql is installed or not
-
 if [ $? -eq 0 ] ; then # if the process is not equal to 0 , It means already installed
     echo "Mysql is already installed ... SKIPPING"
 else # If the process is 0 , Now it will Install the Mysql
     echo "Installing MySql"
     dnf install mysql -y
+    VALIDATE MySql $!
+    
+fi
 
-    if [ $? -ne 0 ]; then
-        echo "Installing MySql is FAILED ..."
-    else
-        echo "Installing MySql is SUCCESS ..."
-    fi
+dnf list installed nginx # here we are cheking the Mysql is installed or not
+if [ $? -eq 0 ] ; then # if the process is not equal to 0 , It means already installed
+    echo "Mysql is already installed ... SKIPPING"
+else # If the process is 0 , Now it will Install the Mysql
+    echo "Installing nginx"
+    dnf install nginx -y
+    VALIDATE nginx $!
+    
 fi
