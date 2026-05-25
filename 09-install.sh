83f3ecc7ffc672961c1e0828c885600c68e9d22d
Dinakar <dinakargaddam23@gmail.com>
2026-05-25 22:17:47 +0530

    special variables

--- 09-install.sh
+++ 09-install.sh
@@ -8,12 +8,17 @@ if [ "$USERID" -ne 0 ]; then
 fi
 
 # echo "I'm continuing ..." 
+dnf list installed mysql # here we are cheking the Mysql is installed or not
 
-echo "Installing MySql"
-dnf install mysql -y
+if [ $? -ne 0 ] ; then # if the process is not equal to 0 , It means already installed
+    echo "Mysql is already installed ... SKIPPING"
+else # If the process is 0 , Now it will Install the Mysql
+    echo "Installing MySql"
+    dnf install mysql -y
 
-if [ $? -ne 0 ]; then
-    echo "Installing MySql is FAILED ..."
-else
-    echo "Installing MySql is SUCCESS ..."
+    if [ $? -ne 0 ]; then
+        echo "Installing MySql is FAILED ..."
+    else
+        echo "Installing MySql is SUCCESS ..."
+    fi
 fi
