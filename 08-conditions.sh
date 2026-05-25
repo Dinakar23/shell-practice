c19bb6f11b386a45e3e465e8a6daf9634dc49f77
Dinakar <dinakargaddam23@gmail.com>
2026-05-25 17:05:06 +0530

    special variables

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-number=$1
-if [ $number -ge 20] then
-    echo "The given $number is greater than or equal to 20"
-    
+NUMBER=$1
+if [ $NUMBER -ge 20]; then
+    echo "The given $NUMBER is greater than or equal to 20"
+
 fi
