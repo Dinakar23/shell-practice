bce5aacd3c966ad96bcf072413bc7c2d08fead0f
Dinakar <dinakargaddam23@gmail.com>
2026-05-26 15:00:16 +0530

    added install script

--- 12-loops.sh
+++ 12-loops.sh
@@ -2,3 +2,25 @@
 
 USERID=$(id -u)
 echo "$USERID"
+
+if [ $? -ne 0 ]; then
+    echo "Please run this command with root access ..."
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -eq o ]; then
+    echo "Mysql is already installed !!!"
+
+else
+    echo "Installling Mysql"
+    dnf install mysql -y
+
+    if [ $? -ne 0 ]; then
+        echo "Installing Failed ..."
+
+    else
+        echo "Installed Successfully ..."
+    fi
+fi
