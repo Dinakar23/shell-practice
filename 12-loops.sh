2ba31502e7c905c1e0ac3b3fa62a669bd85ee665
Dinakar <dinakargaddam23@gmail.com>
2026-05-26 12:58:02 +0530

    special variables

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-#for i in {1..100}
-#do 
-#    echo $i 
-#done
 
 USERID=$(id -u)
 LOGS_DIR=/var/log/shell-script
@@ -10,7 +5,7 @@ LOGS_FILE="$LOGS_DIR/$0.log" #/home/ec2-user/shell-logs/11-install_Pract.sh.log
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ "$USERID" -ne 0 ]; then
-    echon "Please run this command with root access ..."
+    echo "Please run this command with root access ..."
     exit 1
 fi
 
@@ -24,9 +19,9 @@ VALIDATE(){
 
 }
 
-for PACKAGE in $@
+for PACKAGE in "$@"
 do
-    echo "installing $PACKAGE package 
+    echo "installing $PACKAGE package"
     dnf list installed $PACKAGE &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
         dnf install $PACKAGE  -y &>> $LOGS_FILE
@@ -35,4 +30,3 @@ do
         echo "$PACKAGE already installed ... Skipping"
     fi
 done
-
