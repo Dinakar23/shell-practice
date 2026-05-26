82eb66132d2999e830b89ad0d7a2674a9105390e
Dinakar <dinakargaddam23@gmail.com>
2026-05-26 15:36:00 +0530

    added USERLOGS

--- 12-loops.sh
+++ 12-loops.sh
@@ -3,42 +3,45 @@
 USERID=$(id -u)
 #echo "$USERID" Checking whether it is printing 0 or not , Don't need to pring
 
+LOGS_DIR=/home/ec2-user/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+
 if [ "$USERID" -ne 0 ]; then
     echo "Please run this script with root access ..."
     exit 1
 fi
 
 VALIDATE(){
-        if [ $? -ne 0 ]; then
-        echo "Installing $1 Failed ..."
+        if [ $2 -ne 0 ]; then
+        echo "Installing $1 Failed ..."         | tee -a $LOGS_FILE
 
         else
-            echo "Installed $1 Successfully ..."
+            echo "Installed $1 Successfully ..."| tee -a $LOGS_FILE
         fi        
 
 
 }
 
-dnf list installed mysql
+dnf list installed mysql    &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
-    echo "Mysql is already installed !!!"
+    echo "Mysql is already installed !!!"       | tee -a $LOGS_FILE
 
 else
     echo "Installling Mysql"
-    dnf install mysql -y
+    dnf install mysql -y     &>> $LOGS_FILE
     VALIDATE MYSQL $?
     
 fi
 
 
-dnf list installed nginx
+dnf list installed nginx    &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
-    echo "Nginx is already installed..."
+    echo "Nginx is already installed..."        | tee -a $LOGS_FILE
 
 else
     echo "Installing Nginx"
-    dnf install nginx -y
+    dnf install nginx -y    &>> $LOGS_FILE
     VALIDATE nginx $?
 fi
