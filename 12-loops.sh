a60794dc862ed925b77096029d8c630920b624c3
Dinakar <dinakargaddam23@gmail.com>
2026-05-26 12:59:11 +0530

    special variables

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
 
 USERID=$(id -u)
+
 LOGS_DIR=/var/log/shell-script
-LOGS_FILE="$LOGS_DIR/$0.log" #/home/ec2-user/shell-logs/11-install_Pract.sh.log
+
+LOGS_FILE="$LOGS_DIR/$(basename $0).log"
+
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ "$USERID" -ne 0 ]; then
@@ -9,24 +13,40 @@ if [ "$USERID" -ne 0 ]; then
     exit 1
 fi
 
-VALIDATE(){
-    if [ $2 -ne 0 ]; then
-        echo "$TIMESTAMP [Error] Installing $1 Failed ...." | tee -a $LOGS_FILE
-        exit 1
-    else
-        echo "$TIMESTAMP [info] Installed $1 SUCCESS ...." | tee -a $LOGS_FILE
-    fi
 
+VALIDATE(){
+
+    if [ $2 -ne 0 ]; then
+
+        echo "$TIMESTAMP [ERROR] Installing $1 Failed..." | tee -a $LOGS_FILE
+
+        exit 1
+
+    else
+
+        echo "$TIMESTAMP [INFO] Installed $1 SUCCESS..." | tee -a $LOGS_FILE
+
+    fi
 }
+
 
 for PACKAGE in "$@"
 do
-    echo "installing $PACKAGE package"
+
+    echo "Installing $PACKAGE package"
+
     dnf list installed $PACKAGE &>> $LOGS_FILE
+
     if [ $? -ne 0 ]; then
-        dnf install $PACKAGE  -y &>> $LOGS_FILE
+
+        dnf install $PACKAGE -y &>> $LOGS_FILE
+
         VALIDATE "Installing $PACKAGE" $?
+
     else
-        echo "$PACKAGE already installed ... Skipping"
+
+        echo "$PACKAGE already installed...Skipping"
+
     fi
+
 done
