b690a6c660053e5dd814122225f0c8613be43552
Dinakar <dinakargaddam23@gmail.com>
2026-05-26 11:01:10 +0530

    special variables

--- 11-install_Pract.sh
+++ 11-install_Pract.sh
@@ -1,5 +1,6 @@
 USERID=$(id -u)
-
+LOGS_DIR=/home/ec2-user/shell-logs
+LOGS_FILE="$LOGS_DIR/$0.log" #/home/ec2-user/shell-logs/11-install_Pract.sh.log
 if [ "$USERID" -ne 0 ]; then
     echon "Please run this command with root access ..."
     exit 1
@@ -13,20 +14,20 @@ VALIDATE(){
     fi
 
 }
-dnf list installed mysql
+dnf list installed mysql &>> $LOGS_FILE
 if [ $? -eq 0 ]; then
     echo "Its already installed ... SKIPPING"
 else 
     echo "installing Mysql"
-    dnf install mysql -y
-    VALIDATE MySql $2
+    dnf install mysql -y &>> $LOGS_FILE
+    VALIDATE MySql $!
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>> $LOGS_FILE
 if [ $? -eq 0 ]; then
     echo "$1 already installed ... SKIPPING"
 else 
     echo "installing $1"
-    dnf install $1 -y
-    VALIDATE nginx $2
+    dnf install $1 -y &>> $LOGS_FILE
+    VALIDATE nginx $!
 fi
