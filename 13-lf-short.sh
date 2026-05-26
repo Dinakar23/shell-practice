ddfef2b875989a0bbe40ac77efdedfdefaf91ce0
Dinakar <dinakargaddam23@gmail.com>
2026-05-26 16:15:50 +0530

    Changing with some features

---
+++ 13-lf-short.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+USERID=$(id -u)
+#echo "$USERID" Checking whether it is printing 0 or not , Don't need to pring
+
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$( date "+%Y-%m-%d %H:%M:%S")
+
+
+if [ "$USERID" -ne 0 ]; then
+    echo "Please run this script with root access ..."
+    exit 1
+fi
+
+#first argument is what are you trying ot install
+#secong arg -> exit code
+
+VALIDATE(){
+        if [ $2 -ne 0 ]; then
+        echo " $TIMESTAMP [ERROR] Installing $1 Failed ..."         | tee -a $LOGS_FILE
+
+        else
+            echo " $TIMESTAMP [INFO] Installed $1 Successfully ..."| tee -a $LOGS_FILE
+        fi        
+
+}
+
+
+for package in $@ 
+do 
+    echo "Installing Package"
+
+
+done
