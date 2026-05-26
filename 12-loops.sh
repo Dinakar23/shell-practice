61de9202c233ac0ee26b2dec36cbc8c7debaf1a5
Dinakar <dinakargaddam23@gmail.com>
2026-05-26 16:06:11 +0530

    added USERLOGS

--- 12-loops.sh
+++ 12-loops.sh
@@ -5,6 +5,8 @@ USERID=$(id -u)
 
 LOGS_DIR=/var/log/shell-script
 LOGS_FILE="$LOGS_DIR/$0.log"
+TIMESTAMP=$( date "+%Y-%m-%d %H:%M:%S")
+
 
 if [ "$USERID" -ne 0 ]; then
     echo "Please run this script with root access ..."
@@ -13,10 +15,10 @@ fi
 
 VALIDATE(){
         if [ $2 -ne 0 ]; then
-        echo "Installing $1 Failed ..."         | tee -a $LOGS_FILE
+        echo " $TIMESTAMP [ERROR] Installing $1 Failed ..."         | tee -a $LOGS_FILE
 
         else
-            echo "Installed $1 Successfully ..."| tee -a $LOGS_FILE
+            echo " $TIMESTAMP [INFO] Installed $1 Successfully ..."| tee -a $LOGS_FILE
         fi        
 
 
@@ -25,7 +27,7 @@ VALIDATE(){
 dnf list installed mysql &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
-    echo "Mysql is already installed !!!"       | tee -a $LOGS_FILE
+    echo " $TIMESTAMP Mysql is already installed !!!"       | tee -a $LOGS_FILE
 
 else
     echo "Installling Mysql"
@@ -38,10 +40,10 @@ fi
 dnf list installed nginx  &>> $LOGS_FILE
 
 if [ $? -eq 0 ]; then
-    echo "Nginx is already installed..."        | tee -a $LOGS_FILE
+    echo " $TIMESTAMP Nginx is already installed..."        | tee -a $LOGS_FILE
 
 else
-    echo "Installing Nginx"
+    echo " $TIMESTAMP Installing Nginx"
     dnf install nginx -y &>> $LOGS_FILE
     VALIDATE nginx $?
 fi
