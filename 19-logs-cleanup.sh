260918c348a3a0576475924c038e44a0ee908ed2
Dinakar <dinakargaddam23@gmail.com>
2026-05-30 19:17:54 +0530

    To call other script in shell script

--- 19-logs-cleanup.sh
+++ 19-logs-cleanup.sh
@@ -7,3 +7,10 @@ if [ -z $SOURCE_DIR ]; then
     echo "USAGE: $0 <source-dir> [days(optinal to 14 days)]"
     exit 1
 fi
+
+
+if [ -d "$SOURCE_DIR" ]; then
+    echo "Directory exists."
+else
+    echo "Directory does NOT exist."
+fi
