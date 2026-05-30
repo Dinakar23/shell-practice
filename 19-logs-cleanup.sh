ded6440509e37d7613ff1f9447d6e797c54ad3e0
Dinakar <dinakargaddam23@gmail.com>
2026-05-30 19:09:03 +0530

    To call other script in shell script

---
+++ 19-logs-cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SOURCE_DIR=$1
+DAYS=${2:-14}
+
+if [ -z $SOURCE_DIR ]; then
+    ehco "ERROR :: Missing parameters"
+    echo "USAGE: $0 <source-dir> [days(optinal to 14 days)]"
+    exit 1
+fi
